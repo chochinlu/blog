1203d80f2e1ebd0e3d43190f3cdbff661ce241a7
Cho-Ching Lu <chochinlu@gmail.com>
2019-06-04 10:51:15 +0800

    refactor

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import { Box, Card, Heading, Flex, Button } from 'rebass';
-import { StyledReactMarkdown } from './style';
-import { Link } from 'react-router-dom';
+import { StyledReactMarkdown, StyledLink } from './style';
 import styled from 'styled-components/macro';
 import axios from 'axios';
 import { issueUrl, formatedDate, requestConfig } from '../utils';
+
+const ArticleCard = props => (
+  <Card
+    border={2}
+    borderColor="text"
+    px={2}
+    pt={2}
+    pb={4}
+    m={2}
+    color="primaryText"
+  >
+    {props.children}
+  </Card>
+);
+
+const ActionButton = ({ children, ...props }) => (
+  <Button
+    flex={1}
+    bg="text"
+    color="primaryText"
+    fontSize="body1"
+    borderRadius="none"
+    border={1}
+    mx={2}
+    mb={3}
+    {...props}
+  >
+    {children}
+  </Button>
+);
 
 const Article = props => {
   const [fetching, setFetching] = useState(false);
@@ -42,129 +71,86 @@ const Article = props => {
   if (!article && !fetching)
     return (
       <Box flex={1} ref={articleRef}>
-        <Card
-          border={2}
-          borderColor="text"
-          px={2}
-          pt={2}
-          pb={4}
-          m={2}
-          color="primaryText"
-        >
+        <ArticleCard>
           <Heading as="h3" fontSize="h4" ml={2} mt={2}>
             Not Found
           </Heading>
-        </Card>
+        </ArticleCard>
       </Box>
     );
 
   if (!article && fetching)
     return (
       <Box flex={1} ref={articleRef}>
-        <Card
-          border={2}
-          borderColor="text"
-          px={2}
-          pt={2}
-          pb={4}
-          m={2}
-          color="primaryText"
-        >
+        <ArticleCard>
           <Heading as="h3" fontSize="h6" ml={2} mt={2}>
             Fetching Data ....
           </Heading>
-        </Card>
+        </ArticleCard>
       </Box>
     );
 
+  const head = (
+    <>
+      <Heading as="h1" fontSize="h3" color="primaryDark">
+        {article.title}
+      </Heading>
+      <Flex
+        alignItems="center"
+        justifyContent="flex-end"
+        pb={1}
+        css={`
+          border-bottom: 1px solid ${props => props.theme.colors.text};
+        `}
+      >
+        <Heading as="h3" fontSize="body1" mr={2}>
+          Issue {props.match.params.id}
+        </Heading>
+        <Heading as="h3" fontSize="body1" fontWeight={1} mr={2}>
+          {formatedDate(article.updated_at)}
+        </Heading>
+      </Flex>
+    </>
+  );
+
+  const content = (
+    <StyledReactMarkdown
+      source={article.body}
+      color="primaryText"
+      fontSize="body1"
+      fontWeight={1}
+      lineHeight={3}
+      p={2}
+      mt={2}
+    />
+  );
+
+  const actions = (
+    <Flex>
+      <ActionButton>
+        <StyledLink to="/">Back</StyledLink>
+      </ActionButton>
+      <ActionButton
+        onClick={() => window.scrollTo(0, articleRef.current.offsetTop)}
+        css={`
+          cursor: pointer;
+          :focus {
+            outline: none;
+          }
+        `}
+      >
+        Top
+      </ActionButton>
+    </Flex>
+  );
+
   return (
     <Box flex={1} ref={articleRef}>
-      <Card
-        border={2}
-        borderColor="text"
-        px={2}
-        pt={2}
-        pb={4}
-        m={2}
-        color="primaryText"
-      >
-        <Heading as="h1" fontSize="h3" color="primaryDark">
-          {article.title}
-        </Heading>
-        <Flex
-          alignItems="center"
-          justifyContent="flex-end"
-          pb={1}
-          css={`
-            border-bottom: 1px solid ${props => props.theme.colors.text};
-          `}
-        >
-          <Heading as="h3" fontSize="body1" mr={2}>
-            Issue {props.match.params.id}
-          </Heading>
-          <Heading as="h3" fontSize="body1" fontWeight={1} mr={2}>
-            {formatedDate(article.updated_at)}
-          </Heading>
-        </Flex>
-        <StyledReactMarkdown
-          source={article.body}
-          color="primaryText"
-          fontSize="body1"
-          fontWeight={1}
-          lineHeight={3}
-          p={2}
-          mt={2}
-        />
-      </Card>
-      <Flex>
-        <Button
-          flex={1}
-          bg="text"
-          color="primaryText"
-          fontSize="body1"
-          borderRadius="none"
-          border={1}
-          mx={2}
-          mb={3}
-        >
-          <Link
-            to="/"
-            css={`
-              display: inline-block;
-              width: 100%;
-              :link,
-              :visited,
-              :hover,
-              :active {
-                color: inherit;
-              }
-              :focus {
-                outline: none;
-              }
-            `}
-          >
-            Back
-          </Link>
-        </Button>
-        <Button
-          flex={1}
-          bg="text"
-          color="primaryText"
-          fontSize="body1"
-          borderRadius="none"
-          border={1}
-          mx={2}
-          mb={3}
-          onClick={() => window.scrollTo(0, articleRef.current.offsetTop)}
-          css={`
-            :focus {
-              outline: none;
-            }
-          `}
-        >
-          Top
-        </Button>
-      </Flex>
+      <ArticleCard>
+        {head}
+        {content}
+      </ArticleCard>
+      {actions}
     </Box>
   );
 };
