0e795bcc67c1d8c9fbd042bcf881ea55456a8d6e
Cho-Ching Lu <chochinlu@gmail.com>
2019-06-06 11:22:59 +0800

    Add menu-clicked style.

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, Button, Flex } from 'rebass';
 import { Tag } from './style';
 import 'styled-components/macro';
@@ -9,45 +9,57 @@ const tagList = ['React', 'JavaScript', 'CSS', 'NodeJS'];
 const url = label =>
   `https://api.github.com/repos/chochinlu/blog/issues?page=1&per_page=10&labels=${label}`;
 
-const Tags = props => (
-  <Card border={2} borderColor="text" px={2} pt={5} pb={5} m={2}>
-    <Flex mb={2} flexDirection={['row', 'row', 'row', 'column']}>
-      {tagList.map((tag, index) => (
-        <Tag
+const Tags = props => {
+  const [currentTag, setCurrentTag] = useState('all');
+
+  return (
+    <Card border={2} borderColor="text" px={2} pt={5} pb={5} m={2}>
+      <Flex mb={2} flexDirection={['row', 'row', 'row', 'column']}>
+        {tagList.map((tag, index) => (
+          <Tag
+            flex={1}
+            key={tag}
+            color={currentTag === tag ? 'text' : 'primaryText'}
+            bg={currentTag === tag ? 'primary' : 'text'}
+            borderColor="primaryDark"
+            border={1}
+            fontSize="body1"
+            borderRadius="none"
+            mb={[index === tagList.length - 1 ? 0 : 2, 1]}
+            onClick={e => {
+              props.setUrl(url(tag));
+              setCurrentTag(tag);
+            }}
+          >
+            <StyledLink to="/">{tag}</StyledLink>
+          </Tag>
+        ))}
+      </Flex>
+      <Flex>
+        <Button
           flex={1}
-          key={tag}
-          color="primaryText"
-          bg="text"
-          border={1}
-          fontSize="body1"
           borderRadius="none"
-          mb={[index === tagList.length - 1 ? 0 : 2, 1]}
-          onClick={e => props.setUrl(url(tag))}
+          fontSize="body1"
+          color={currentTag === 'all' ? 'text' : 'primaryText'}
+          bg={currentTag === 'all' ? 'primary' : 'text'}
+          borderColor="primaryDark"
+          border={1}
+          onClick={e => {
+            props.setUrl(props.initUrl);
+            setCurrentTag('all');
+          }}
+          css={`
+            cursor: pointer;
+            :focus {
+              outline: none;
+            }
+          `}
         >
-          <StyledLink to="/">{tag}</StyledLink>
-        </Tag>
-      ))}
-    </Flex>
-    <Flex>
-      <Button
-        flex={1}
-        borderRadius="none"
-        fontSize="body1"
-        bg="text"
-        color="primaryText"
-        border={1}
-        onClick={e => props.setUrl(props.initUrl)}
-        css={`
-          cursor: pointer;
-          :focus {
-            outline: none;
-          }
-        `}
-      >
-        所有內容
-      </Button>
-    </Flex>
-  </Card>
-);
+          所有內容
+        </Button>
+      </Flex>
+    </Card>
+  );
+};
 
 export default Tags;
